308bd3aaf01e4d2423f1f60d17989f172639993f
NarasimhamMantha <narasimham.mantha@tcs.com>
2024-07-02 15:00:37 +0100

    testload

--- test/exec_sqlcmd_june24.sh
+++ test/exec_sqlcmd_june24.sh
@@ -21,7 +21,7 @@ bcp kafkaeunnprodsqldb0001.dbo.daily_trans_topics_data_sent_prod_public      in 
 bcp kafkaeunnprodsqldb0001.dbo.daily_trans_topics_data_recv_prod_public      in prod-public_receivedbytes_June2024_uniq_final.csv  -S  $SQL_SERVER -U $SQL_USER -P $SQL_PASSWORD  -q -c -t ","
 bcp kafkaeunnprodsqldb0001.dbo.daily_trans_topics_data_retained_prod_public  in prod-public_retainedbytes_June2024_uniq_final.csv  -S  $SQL_SERVER -U $SQL_USER -P $SQL_PASSWORD  -q -c -t ","
 
-
+bcp kafkaeunnprodsqldb0001.dbo.daily_base  in  Daily_June2024.csv  -S  $SQL_SERVER -U $SQL_USER -P $SQL_PASSWORD  -q -c -t ","
 echo "==== exec update_daily_trans_topics_data_sent_recv_retained ====="
 sqlcmd -S $SQL_SERVER -U $SQL_USER -P $SQL_PASSWORD -d kafkaeunnprodsqldb0001 -q "exec update_daily_trans_topics_data_sent_recv_retained; " < /dev/null
 
